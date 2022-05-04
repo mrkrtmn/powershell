f7743b8fae82e64ce5420e966fd1fd7e04c96475
mrkrtmn <104874239+mrkrtmn@users.noreply.github.com>
2022-05-03 20:07:28 -0400

    Add files via upload

---
+++ copy files remotely.ps1
@@ -0,0 +1,16 @@
+﻿# This file contains the list of servers you want to copy files/folders to
+$computers = gc "C:\Users\falcon010\Desktop\lsinfoactiva.txt"
+ 
+# This is the file/folder(s) you want to copy to the servers in the $computer variable
+$source = "C:\Users\falcon010\Desktop\INFOACTIVAClienteBISA.exe.config"
+ 
+# The destination location you want the file/folder(s) to be copied to
+$destination = "C$\"
+ 
+foreach ($computer in $computers) {
+if ((Test-Path -Path \\$computer\$destination)) {
+Copy-Item $source -Destination \\$computer\$destination -Recurse
+} else {
+"\\$computer\$destination is not reachable or does not exist"
+}
+}
