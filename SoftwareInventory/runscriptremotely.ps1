baa05c442a1fdd8698f8abbe0cf97fdf1b0db86f
mrkrtmn <104874239+mrkrtmn@users.noreply.github.com>
2022-05-03 21:28:27 -0400

    update

---
+++ SoftwareInventory/runscriptremotely.ps1
@@ -0,0 +1,7 @@
+﻿Invoke-command -ComputerName (get-content C:\Users\falcon010\Desktop\slprb.txt) -filepath C:\Users\falcon010\Desktop\softwareinstalled.ps1
+$computers = gc "C:\Users\falcon010\Desktop\slprb.txt"
+foreach ($computer in $computers) {
+Move-Item -Path "\\$computer\c$\*.html" -Destination "F:\reports\"
+}
+
+
---
+++ SoftwareInventory/softwareinstalled.ps1
@@ -0,0 +1,4 @@
+﻿$hostname = hostname
+$INSTALLED = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+$INSTALLED += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+$INSTALLED | ?{ $_.DisplayName -ne $null } | sort-object -Property DisplayName -Unique | ConvertTo-Html | Out-File "C:\$hostname.html"
