2472e677a92d4a1aaf4b6262cc0e71ca92a64aa2
mrkrtmn <104874239+mrkrtmn@users.noreply.github.com>
2022-05-03 20:10:42 -0400

    Add files via upload

---
+++ ip to name.ps1
@@ -0,0 +1,7 @@
+﻿$IPFILE = Get-Content C:\Users\falcon010\Desktop\list.txt
+$results=@()
+foreach ($IP in $IPFILE){
+$o=Resolve-DNSName $IP | Select-Object -Property NameHost, @{name='IP';expression={$IP}}
+$results+=$o
+}
+$results
