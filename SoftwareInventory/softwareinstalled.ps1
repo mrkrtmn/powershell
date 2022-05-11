c67d96d0d88e7da37ba564e35bc970bfad04ea82
mrkrtmn <104874239+mrkrtmn@users.noreply.github.com>
2022-05-10 20:21:05 -0400

    Update softwareinstalled.ps1

--- SoftwareInventory/softwareinstalled.ps1
+++ SoftwareInventory/softwareinstalled.ps1
@@ -3,4 +3,4 @@
 $hostname = hostname
 $INSTALLED = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
 $INSTALLED += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
-$INSTALLED | ?{ $_.DisplayName -ne $null } | sort-object -Property DisplayName -Unique | ConvertTo-Html | Out-File "C:\tempfolderbs\$hostname.html"
+$INSTALLED | ?{ $_.DisplayName -ne $null } | sort-object -Property DisplayName -Unique | ConvertTo-Html | Out-File "C:\$hostname.html"
