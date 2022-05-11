1da3b94143b6b772faf69b5c411388b66b314085
mrkrtmn <104874239+mrkrtmn@users.noreply.github.com>
2022-05-10 20:23:56 -0400

    Delete softwareinstalled.ps1

--- SoftwareInventory/softwareinstalled.ps1
+++
@@ -1,6 +0,0 @@
-#Set-Service -Name WinRm -StartupType Automatic
-#Restart-Service WinRm
-$hostname = hostname
-$INSTALLED = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
-$INSTALLED += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
-$INSTALLED | ?{ $_.DisplayName -ne $null } | sort-object -Property DisplayName -Unique | ConvertTo-Html | Out-File "C:\$hostname.html"
