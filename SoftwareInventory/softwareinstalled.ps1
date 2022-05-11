538380747cad3fedd11829265566cf26b11a633d
mrkrtmn <104874239+mrkrtmn@users.noreply.github.com>
2022-05-10 20:19:25 -0400

    Update softwareinstalled.ps1

--- SoftwareInventory/softwareinstalled.ps1
+++ SoftwareInventory/softwareinstalled.ps1
@@ -1,5 +1,5 @@
-﻿Set-Service -Name WinRm -StartupType Automatic
-Restart-Service WinRm
+#Set-Service -Name WinRm -StartupType Automatic
+#Restart-Service WinRm
 $hostname = hostname
 $INSTALLED = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
 $INSTALLED += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
