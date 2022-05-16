74132c114482f45fbd1a9cb8069169ce20bca976
mrkrtmn <104874239+mrkrtmn@users.noreply.github.com>
2022-05-15 22:31:06 -0400

    Create inventory_vms.ps1

---
+++ inventory_vms.ps1
@@ -0,0 +1,8 @@
+﻿#invetory of VM´s
+#$hosts = Get-Content "d:\hostslist.txt"
+Get-VM -ComputerName $hosts | Where { $_.State –eq ‘Running’ } | select ComputerName,name,state | Export-Csv -Path d:\VMList-$((Get-Date).ToString("MM-dd-yyyy")).csv -NoTypeInformation
+
+
+
+
+
